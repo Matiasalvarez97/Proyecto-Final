902c2734e39b8a38261cc1435a8420c5435ac12c
matiasalvarez <matias_alvarez97@hotmail.com>
2026-02-16 22:39:56 -0300

    add: se añadio mensaje de no encontrado al filtro y se uso api para buscarusers

--- src/assets/components/Aside.jsx
+++ src/assets/components/Aside.jsx
@@ -1,20 +1,45 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { users } from "../../services/mockApi.js"
 const Aside = () => {
     const [search , setSearch] = useState ("")
+    const [users, setUsers] = useState([])
     const handleChange = (event) => {
         setSearch(event.target.value)
     }
-    const filteredUsers = users.filter((user) => user.name.toLowerCase(). includes (search.toLocaleLowerCase()))
+
+    const fectchingData = async () => {
+        try{
+            const response = await fetch("https://dummyjson.com/users")
+            if (!response.ok){
+                alert("fallo el pedido")
+                return
+            }
+            const data = await response.json()
+             setUsers(data.users)
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fectchingData()
+    },[])
+
+    const filteredUsers = users.filter((user) => user.firstName.toLowerCase(). includes (search.toLocaleLowerCase()))
   return (
         <aside>
             <h1>Chat UTN</h1>
             <input type="search" placeholder="Buscar Contactos" onChange={handleChange}/>
+            {
+                filteredUsers.length === 0 && <p>No se encontraron contactos</p> 
+            }
             <ul>
                 {
                    filteredUsers.map ((user) => (
-                    <li>{user.name}
-                    <small>{user.status}</small>
+                    <li key={user.id}>
+                        <img src={user.image} alt="foto de perfil" />
+                        <div>{user.firstName} {user.lastName}</div>
+                    <small>{user.address.country}</small>
                     </li>
                   
                    )
--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -1,9 +1,9 @@
   const users = [
-    { name: "Lionel Messi " , status: "online" } ,
-    { name: "Taylor S" , status: "online" } ,
-    { name: "Drew B" , status: "online" } ,
-    { name: "Liam G" , status: "offline" } ,
-    { name: "Noel G" , status: "online" } ,
+    { id: 1,name: "Lionel Messi " , status: "online" } ,
+    { id: 2,name: "Taylor S" , status: "online" } ,
+    { id: 3,name: "Drew B" , status: "online" } ,
+    { id: 4,name: "Liam G" , status: "offline" } ,
+    { id: 5,name: "Noel G" , status: "online" } ,
    ];
 
    export {users}
